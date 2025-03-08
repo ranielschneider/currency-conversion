816ab73d7d9a9b4b7e5cc9c55eb88b0e888f8fc3
paulamcs <paula-mcs@hotmail.com>
2025-03-08 19:57:34 +0100

    Criado Toggle Button + Espaco para logo no cabecalho + Dropdown menu para bandeiras nos Spinners

--- app/src/main/java/com/example/hacksprint/MainActivity.kt
+++ app/src/main/java/com/example/hacksprint/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+            val toggleGroup= findViewById<MaterialButtonToggleGroup>(R.id.toggleGroup)
+            toggleGroup.addOnButtonCheckedListener(object : MaterialButtonToggleGroup.OnButtonCheckedListener {
+                override fun onButtonChecked(group: MaterialButtonToggleGroup, checkedId: Int, isChecked: Boolean) {
+                    when (checkedId) {
+                        R.id.buttonConverter -> {
+                            if (isChecked) {
+                            } else {
+                            }
+                        }
+                        R.id.buttonGraphic -> {
+                            if (isChecked) {
+                            } else {
+                            }
+                        }
+                    }
+                }
+            }  )
+        }
     }
-}
+
+
