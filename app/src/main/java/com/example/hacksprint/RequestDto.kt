4cd091baae6f9e564ca67970227aaf5ad1d462f9
nicolas-maloney <nicolasmaloneyf@gmail.com>
2025-03-11 10:19:39 -0300

    "Added kotlinx serialization"

--- app/src/main/java/com/example/hacksprint/RequestDto.kt
+++ app/src/main/java/com/example/hacksprint/RequestDto.kt
@@ -1,6 +1,9 @@
 package com.example.hacksprint
 
-class Request {
+
+@kotlinx.serialization.Serializable
+
+data class Request {
     var result: String,
     var time_last_update_utc: String,
     var time_next_update_utc: String,
